35bd83f57f406baf253a1406c0414a225e4a43ba
igrzhrnk <igorzhrnko@gmail.com>
2023-12-21 15:15:57 +0200

    add route and view for main page

--- racoon_project/urls.py
+++ racoon_project/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 from .yasg import urlpatterns as doc_urls
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", view=views.index, name="home")
 ]
 
 # Include URL patterns for API documentation
