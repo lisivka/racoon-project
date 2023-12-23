58a726caec82787011fc54d25fe0bd2d5fc131e6
lisivka <lisivka@gmail.com>
2023-12-23 19:42:25 +0200

    add actions and visualisations for admin.py

--- apps/auction/admin.py
+++ apps/auction/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
 from .models import AuctionsModel
+from django.core.serializers import serialize
+
+
+@admin.action(description="Mark selected as NOT active")
+def make_not_active(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+
+@admin.action(description="Mark selected as active")
+def make_active(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+
 
 
 @admin.register(AuctionsModel)
@@ -8,5 +21,14 @@ class AuctionsAdmin(admin.ModelAdmin):
     """Admin configuration for the AuctionsModel."""
 
     verbose_name_plural = "Auctions"
-    list_display = ('lot', 'is_active', 'start_date', 'end_date')
+    list_display = ('id','lot', 'is_active', 'start_date', 'end_date')
+    list_display_links = ('id','lot',  'start_date', 'end_date')
+    list_editable = ('is_active',)
     search_fields = ('lot', 'is_active')
+    list_filter = ['is_active',]
+
+    actions = [make_active,
+               make_not_active,
+               ]
+
+
--- apps/lot/admin.py
+++ apps/lot/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
-from .models import VehicleModel, ColorModel, EngineModel, FuelModel, ConditionModel, \
-    BrandModel, ModelModel, LotModel, PhotoModel
+from .models import VehicleModel, ColorModel, EngineModel, FuelModel, ConditionModel
+from .models import BrandModel, ModelModel, LotModel, PhotoModel
+
+@admin.action(description="Mark selected as NOT active")
+def make_not_active(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+
+@admin.action(description="Mark selected as active")
+def make_active(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
 
 admin.site.register(VehicleModel)
 admin.site.register(ColorModel)
@@ -10,5 +20,37 @@ admin.site.register(FuelModel)
 admin.site.register(ConditionModel)
 admin.site.register(BrandModel)
 admin.site.register(ModelModel)
-admin.site.register(LotModel)
-admin.site.register(PhotoModel)
+# admin.site.register(PhotoModel)
+
+@admin.register(LotModel)
+class LotAdmin(admin.ModelAdmin):
+    """Admin configuration for the LotModel."""
+
+    verbose_name_plural = "Lots"
+    list_display = ('id', 'owner', 'vehicle', 'is_active', 'fuel', 'condition', 'state', 'color')
+    list_display_links = ('id', 'owner', 'vehicle')
+    list_editable = ('is_active',)
+    search_fields = ('owner', 'vehicle', 'is_active' )
+    list_filter = ('is_active','fuel', 'condition', 'state', 'color', 'vehicle')
+
+    # @admin.action(description="Mark selected as NOT active")
+    # def make_not_active(modeladmin, request, queryset):
+    #     queryset.update(is_active=False)
+    #
+    # @admin.action(description="Mark selected as active")
+    # def make_active(modeladmin, request, queryset):
+    #     queryset.update(is_active=True)
+
+    actions = [make_not_active,
+               make_active,
+               ]
+
+@admin.register(PhotoModel)
+class PhotoAdmin(admin.ModelAdmin):
+    """Admin configuration for the PhotoModel."""
+
+    verbose_name_plural = "Photos"
+    list_display = ('id', 'lot', 'url')
+    list_display_links = ('id', 'lot', 'url')
+    search_fields = ('lot', 'photo')
+
