bb4ef9cbd1284b2f225db9b89a019f8a18695f06
Lisivka <lisivka@gmail.com>
2023-12-27 14:44:40 +0200

    refactoring views.py for lot

--- apps/lot/urls.py
+++ apps/lot/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view=views.show_lots, name="lot-list"),
-    path("/<int:pk>", view=views.lot_detail, name="lot-detail"),
+    path("", view=views.lot_list, name="lot_list"),
+    path("/<int:pk>", view=views.lot_details, name="lot_details"),
 ]
--- apps/lot/views.py
+++ apps/lot/views.py
@@ -1,22 +1,38 @@
 from django.template import loader
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http.response import HttpResponse
+from .services import LotServices
 
 from .models import LotModel
 
+def get_offset_limit_page(page):
+    page_prev = page - 1
+    page_next = page + 1
+    if page <= 1:
+        page = 1
+        page_prev = 1
+    LIMIT = 20
+    OFFSET = page * LIMIT - LIMIT
+    return OFFSET, LIMIT, page, page_prev, page_next
 
-def show_lots(request):
-    tmpl = loader.get_template("lots.html")
-    lots = LotModel.objects.all()
-    content = {
-        "lot_list": lots
-    }
-    return HttpResponse(tmpl.render(content, request))
+def lot_list(request, page=1):
+    context = {}
+    context["title"] = "Lots"
+    offset, limit, page, page_prev, page_next = get_offset_limit_page(page)
+    lots = LotServices().get_all()
+    context["lots"] = lots[offset:offset + limit]
+    context["page"] = page
+    context["page_next"] = page_next
+    context["page_prev"] = page_prev
+
+    return render(request, "lot/lots.html", context=context)
 
 
-def lot_detail(request, pk):
-    tmpl = loader.get_template("lot_info.html")
-    lot = LotModel.objects.get(id=pk)
-    content = {
-        "lot_detail": lot
-    }
-    return HttpResponse(tmpl.render(content, request))
+def lot_details(request, pk):
+
+    lot = LotServices().get_by_id(pk)
+    context = {}
+    context["title"] = "Lot Details"
+    context["lot"] = lot
+
+    return render(request, "lot/lot_details.html", context=context)
