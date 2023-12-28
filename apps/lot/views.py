6cce2adcba498fd3c4f88ef1e4a856577cc31a6d
igrzhrnk <igorzhrnko@gmail.com>
2023-12-28 19:04:07 +0200

    add CRUD for lot

--- apps/lot/views.py
+++ apps/lot/views.py
@@ -1,38 +1,47 @@
-from django.template import loader
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http.response import HttpResponse
+from django.shortcuts import render
 from .services import LotServices
-
-from .models import LotModel
-
-def get_offset_limit_page(page):
-    page_prev = page - 1
-    page_next = page + 1
-    if page <= 1:
-        page = 1
-        page_prev = 1
-    LIMIT = 20
-    OFFSET = page * LIMIT - LIMIT
-    return OFFSET, LIMIT, page, page_prev, page_next
-
-def lot_list(request, page=1):
-    context = {}
-    context["title"] = "Lots"
-    offset, limit, page, page_prev, page_next = get_offset_limit_page(page)
-    lots = LotServices().get_all()
-    context["lots"] = lots[offset:offset + limit]
-    context["page"] = page
-    context["page_next"] = page_next
-    context["page_prev"] = page_prev
-
-    return render(request, "lot/lots.html", context=context)
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import LotSerializer, PhotoSerializer
+from .models import LotModel, PhotoModel
 
 
-def lot_details(request, pk):
+class LotListsView(ListCreateAPIView):
+    queryset = LotServices().get_all()
+    serializer_class = LotSerializer
+    # permission_classes = (IsAuthenticated,)
 
-    lot = LotServices().get_by_id(pk)
-    context = {}
-    context["title"] = "Lot Details"
-    context["lot"] = lot
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = LotSerializer(queryset, many=True)
+        return render(request, "lot/lots.html", {"lot_list": serializer.data})
 
-    return render(request, "lot/lot_details.html", context=context)
+
+class LotView(RetrieveUpdateDestroyAPIView):
+    queryset = LotServices().get_all()
+    serializer_class = LotSerializer
+    # permission_classes = (IsAuthenticated,)
+
+    def retrieve(self, request, pk):
+        lot = LotServices().get_by_id(pk)
+        photo = PhotoModel.objects.get(lot_id=lot.id)
+        lot_serializer = LotSerializer(lot)
+        photo_serializer = PhotoSerializer(photo)
+        return render(
+            request,
+            "lot/lot_details.html",
+            {
+                "lot": lot_serializer.data,
+                "photo": photo_serializer.data
+            })
+
+
+# def get_offset_limit_page(page):
+#     page_prev = page - 1
+#     page_next = page + 1
+#     if page <= 1:
+#         page = 1
+#         page_prev = 1
+#     LIMIT = 20
+#     OFFSET = page * LIMIT - LIMIT
+#     return OFFSET, LIMIT, page, page_prev, page_next
