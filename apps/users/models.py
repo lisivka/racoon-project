0f3700fa06583fced50fb3dc812b190d349fe943
Lisivka <lisivka@gmail.com>
2023-12-28 18:07:05 +0200

    delete field is_active

--- apps/users/models.py
+++ apps/users/models.py
@@ -9,7 +9,6 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
         db_table = 'auth_user'
 
     email = models.EmailField(unique=True)
-    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
