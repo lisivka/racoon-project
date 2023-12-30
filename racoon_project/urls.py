5998f20f8a9f5c8167d439da86aa1f7fbbcc48d8
lisivka <lisivka@gmail.com>
2023-12-30 14:43:05 +0200

    update url

--- racoon_project/urls.py
+++ racoon_project/urls.py
@@ -2,9 +2,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from apps.auction.views import AuctionViewSet
+from apps.bid.views import BidViewSet
+from apps.lot.views import ColorViewSet, LotViewSet, VehicleViewSet
 
 from . import views
 from .yasg import urlpatterns as doc_urls
+
+router = DefaultRouter()
+router.register(r'bids', BidViewSet, basename="bids")
+router.register(r'auctions', AuctionViewSet, basename="auctions")
+router.register(r'lots', LotViewSet, basename="lots")
+router.register(r'colors', ColorViewSet, basename="colors")
+router.register(r'vehicles', VehicleViewSet, basename="vehicles")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,10 +26,14 @@ urlpatterns = [
 
     path('', view=views.index, name='home'),
 
-    path('api/v1/users/', include('apps.users.urls')),
-    path('api/v1/auctions/', include('apps.auction.urls')),
-    path('api/v1/lots/', include('apps.lot.urls')),
-    path('api/v1/auctions/', include('apps.bid.urls')),
+    path('users', include('apps.users.urls')),
+    path('auctions', include('apps.auction.urls')),
+    path('lots', include('apps.lot.urls')),
+    path('auctions/', include('apps.bid.urls')),
+
+    # for API
+    path('api/v1/', include(router.urls)),
+
 ]
 
 # Serve media files in development
