e959d0e0c4f216899553c1d36c421d86a38b6b6c
VitaliiKosyk <kostamtik@gmail.com>
2024-01-03 00:20:31 +0200

    - Dockerfile  - docker-compose.yml rewrite README.md add postgres add redis createsuperuser load dump db

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wait_for_postgres() {
+    until nc -z "$DATABASE_HOST" "$DATABASE_PORT"; do
+        echo "PostgresSQL is not available. Waiting..."
+        sleep 2
+    done
+    echo "PostgresSQL started"
+}
+
+wait_for_postgres
+echo "PostgreSQL is ready. Proceed with your script."
+
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py createsuperuser --no-input
+python manage.py loaddata db.json
+
+
+echo "Starting Server..."
+python manage.py runserver 0.0.0.0:8000
