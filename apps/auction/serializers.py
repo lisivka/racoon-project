3f985f055be97e83ce202c4d60de2d7595303adf
lisivka <lisivka@gmail.com>
2024-01-03 22:50:32 +0200

    extend serializers.py

--- apps/auction/serializers.py
+++ apps/auction/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 
 from .models import Auction
+from  apps.lot.serializers import LotSerializers
+
 
 
 class AuctionsSerializers(ModelSerializer):
+    lot = LotSerializers()
     class Meta:
         model = Auction
         fields = '__all__'
