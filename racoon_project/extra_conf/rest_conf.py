6ce9b2dd11dcd3a780d01e806f18950f6501907e
lisivka <lisivka@gmail.com>
2024-01-03 22:48:54 +0200

    add SessionAuthentication

--- racoon_project/extra_conf/rest_conf.py
+++ racoon_project/extra_conf/rest_conf.py
@@ -5,6 +5,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
