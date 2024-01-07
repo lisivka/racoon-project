3340dc38321347297866914be77989fa4776d75a
lisivka <lisivka@gmail.com>
2024-01-07 22:30:24 +0200

    add STATICFILES_DIRS

--- racoon_project/settings.py
+++ racoon_project/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 # Extra places for collect static files.
 
 MEDIA_URL = '/media/'
